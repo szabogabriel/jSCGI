6209145bf2b1c54ec1960ade87bac370aff6fcfb
Gabriel Szabo <szabo.gabriel@gmail.com>
2016-04-29 08:09:41 +0200

    JSCGI Server can listen on a host.

--- src/com/jscgi/JSCGIServer.java
+++ src/com/jscgi/JSCGIServer.java
@@ -1,6 +1,7 @@
 package com.jscgi;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,12 +11,22 @@ public class JSCGIServer implements Runnable {
 	private final int PORT;
 	private final ExecutorService THREAD_POOL;
 	private final ServerSocket SERVER_SOCKET;
+	private final String HOST;
 	private final JSCGIRequestHandlerFactory REQUEST_HANDLER_FACTORY;
 	
 	public JSCGIServer(int port, JSCGIRequestHandlerFactory handlerFactory) throws IOException {
 		this.PORT = port;
+		this.HOST = "";
 		this.THREAD_POOL = Executors.newCachedThreadPool();
 		this.SERVER_SOCKET = new ServerSocket(this.PORT);
+		this.REQUEST_HANDLER_FACTORY = handlerFactory;
+	}
+	
+	public JSCGIServer(String host, int port, JSCGIRequestHandlerFactory handlerFactory) throws IOException {
+		this.PORT = port;
+		this.HOST = host;
+		this.THREAD_POOL = Executors.newCachedThreadPool();
+		this.SERVER_SOCKET = new ServerSocket(this.PORT, 0, InetAddress.getByName(this.HOST));
 		this.REQUEST_HANDLER_FACTORY = handlerFactory;
 	}
 	
